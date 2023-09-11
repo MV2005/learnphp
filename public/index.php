39580414fce896c354ff04794b8108c3928aa083
Marleen Vahtra <marleenvahtra@gmail.com>
2023-09-11 13:51:08 +0300

    somthing

--- public/index.php
+++ public/index.php
@@ -1,3 +1,25 @@
 <?php
 
-var_dump($_SERVER);
+spl_autoload_register(function ($className){
+ var_dump($className);
+ require_once __DIR__ ."/../src/$className.php";
+});
+
+//require_once __DIR__ .'/../src/Router.php';
+
+
+$router = new Router();
+var_dump($router);
+
+switch ($_SERVER['REQUEST_URI']) {
+   case '/':
+    echo 'Home page';
+    break;
+    case '/about';
+    echo 'About';
+    break;
+    default:
+    echo '404';
+    break;
+
+}
