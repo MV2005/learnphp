4951e5847347829d6e0a50fee63bd426a88598af
Marleen Vahtra <marleenvahtra@gmail.com>
2023-09-11 15:09:06 +0300

    Add Controllers

--- public/index.php
+++ public/index.php
@@ -1,25 +1,50 @@
 <?php
 
 spl_autoload_register(function ($className){
- var_dump($className);
+ $className = substr($className, 4);
  require_once __DIR__ ."/../src/$className.php";
 });
 
 //require_once __DIR__ .'/../src/Router.php';
 
+use App\Router;
 
-$router = new Router();
-var_dump($router);
+require __DIR__ . '/../routes.php';
 
-switch ($_SERVER['REQUEST_URI']) {
-   case '/':
-    echo 'Home page';
-    break;
-    case '/about';
-    echo 'About';
-    break;
-    default:
-    echo '404';
-    break;
 
-}
+//    var_dump(Router::$routes);
+
+   $router = new Router($_SERVER['REQUEST_URI']);
+   $match = $router->match();
+   if($match){
+    if(is_callable($match['action'])){
+    call_user_func($match['action']);
+   } else if(is_array($match['action'])){
+    $class = $match['action'][0]; 
+    $controller = new $class();
+    $method = $match['action'][1];
+    $controller->$method();
+
+   }
+} else  {
+    echo 404;
+   }
+
+
+
+
+// $router = new Router();
+// var_dump($router);
+
+// switch ($_SERVER['REQUEST_URI']) {
+//    case '/':
+//     echo 'Home page';
+//     break;
+//     case '/about';
+//     echo 'About';
+//     break;
+//     default:
+//     echo '404';
+//     break;
+
+// }
